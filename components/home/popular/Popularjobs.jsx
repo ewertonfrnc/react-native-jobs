1c72752c3bea6acd4fac50d14c65f6e20a9ac7b2
Ewerton Franco <ewerton.silva@usemobile.xyz>
2023-04-01 20:33:47 -0300

    feat: added a fetch hook and updated the popular jobs component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -8,13 +8,45 @@ import {
 } from "react-native";
 import { useRouter } from "expo-router";
 
+import useFetch from "../../../hooks/useFetch";
+
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+
 import { COLORS, SIZES } from "../../../constants";
 import styles from "./popularjobs.style";
 
 const Popularjobs = () => {
+  const router = useRouter();
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: 1,
+  });
+
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Em destaque</Text>
+        <TouchableOpacity onPress={() => {}}>
+          <Text style={styles.headerBtn}>Ver mais</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Algo deu errado...</Text>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          />
+        )}
+      </View>
     </View>
   );
 };
