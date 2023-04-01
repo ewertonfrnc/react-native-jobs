562850fbc06505fcf41525435561327b59fc81af
Ewerton Franco <ewerton.silva@usemobile.xyz>
2023-04-01 19:21:17 -0300

    feat: setup fonts

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './welcome.style'
+import { icons, SIZES } from "../../../constants";
+
+import styles from "./welcome.style";
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Olá, Ewerton!</Text>
+        <Text style={styles.welcomeMessage}>Encontre seu próximo trabalho</Text>
+      </View>
 
-export default Welcome
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
